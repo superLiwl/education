79846869f6b9b0dbcbe06331e14f4594edef2837
superLiwl <lwl.286131719@163.com>
2019-06-03 17:22:11 +0800

    初始化代码

---
+++ aldd/src/main/java/com/aldd/login/AppLoginController.java
@@ -0,0 +1,16 @@
+package com.aldd.login;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * AppLoginController
+ * @author liwl
+ * @version 2019-04-18
+ */
+@Controller
+@RequestMapping(value = "${frontPath}/shop/shop")
+public class AppLoginController {
+	
+	
+}
